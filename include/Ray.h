9a715228d4b666a7b371124c5d2191fa6ef9fa91
hasting7 <hasting7@purdue.edu>
2026-03-30 14:21:31 -0400

    added basic rays

---
+++ include/Ray.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#include "Vec3.h"
+
+struct Ray {
+	Ray (const Vec3& origin, const Vec3& direction) : origin(origin), dir(direction) {};
+	Ray() : Ray(Vec3(), Vec3()) {};
+	Vec3 origin;
+	Vec3 dir;
+};
