c50c1f973df7835a76dce0311f83a4ce6f209d15
hasting7 <hasting7@purdue.edu>
2026-05-04 22:04:14 -0400

    clean scene

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,16 @@
 
 // ARGUMENTS
 
-static const int   width = 400;
-static const int   height = 300;
+static const int   width = 800;
+static const int   height = 600;
 static const int   threads = 12;
+
 static const int   max_ray_depth = 100;
-static const int   rays_per_pixel = 100;
+static const int   rays_per_pixel = 500;
+
+static const Vec3  camera_origin = Vec3(0, 3, 10);
 static const float aperture = 0.08;
-static const float focal_distance = 12;
+static const float focal_distance = 10;
 
 Environment environment = Environment(width, height, threads, max_ray_depth, rays_per_pixel);
 
@@ -28,19 +31,39 @@ void make_square(Vec3 bottom_left, Vec3 bottom_right, Material mat);
 
 void initialize_scene() {
     // set camera settings
+    environment.update_camera_position(camera_origin);
+    environment.update_camera_settings(aperture, focal_distance);
+
+    environment.update_camera_position(camera_origin);
     environment.update_camera_settings(aperture, focal_distance);
 
 
-    Material floor(Color(0.9f,0.9f,0.9f), 1.0f, 0.1f);
+    Material floor(Color(0.95f, 0.95f, 0.95f), 0.2f, 0.0f);
     environment.add_object(std::make_shared<Plane>(
-        Vec3(0,0,0), Vec3(0,1,0), floor
+        Vec3(0, 0, 0), Vec3(0, 1, 0), floor
     ));
 
-    Material light(Color(0.9f,0.05f,0.05f), 0.0f, 0.0f);
-    light.emission_strength = 0.9;
-    light.emission_color = Color(0.9f, 0.05f, 0.05f);
+    Material wall(Color(0.95f, 0.95f, 0.95f), 0.3f, 0.0f);
+    make_square(
+        Vec3(-20, 0, -14),
+        Vec3( 5, 0, -14),
+        wall
+    );
+
+    Material glass(Color(1.0f, 1.0f, 1.0f), 1.0f, 1.0f);
+    glass.transmission = 1;
+    glass.refractive_index = 1.52f;
+    glass.glass_roughness = 0.0f;
+    glass.tint = Color(1.0f, 1.0f, 1.0f);
     environment.add_object(std::make_shared<Sphere>(
-        Vec3(0,0.5f,0), 0.5f, light
+        Vec3(-8, 4, -6), 4, glass
+    ));
+
+    Material light(Color(1.0f, 1.0f, 1.0f), 0.0f, 0.0f);
+    light.emission_strength = 1.0f;
+    light.emission_color = Color(0.99f, 0.99f, 0.99f);
+    environment.add_object(std::make_shared<Sphere>(
+        Vec3(-40, 30, 10), 8, light
     ));
 }
 
